096493d7eadb2125a0081a3cfd2af83fb520f4ec
rwiv <zc798546@gmail.com>
2025-01-17 18:48:30 +0900

    first commit

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,11 @@
+import {css} from "@emotion/react";
+
+const style = css`
+  color: hotpink;
+`;
+
+export function IndexPage() {
+  return (
+    <div className="m-3" css={style}>hello</div>
+  )
+}
